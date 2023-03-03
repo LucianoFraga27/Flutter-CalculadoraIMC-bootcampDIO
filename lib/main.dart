0b4c55fe3c13bb8e32b1456543d98463294869e2
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-03-03 11:08:16 -0300

    Calculadora IMC com Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: CalculadoraIMC(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
+class CalculadoraIMC extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  _CalculadoraIMCState createState() => _CalculadoraIMCState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'flutter demo teste',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'Teste'),
-    );
-  }
-}
+class _CalculadoraIMCState extends State<CalculadoraIMC> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  double _imc = 0.0;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _calculateIMC() {
+    double height = double.parse(_heightController.text) / 100.0;
+    double weight = double.parse(_weightController.text);
+    double imc = weight / (height * height);
     setState(() {
-      _counter++;
+      _imc = imc;
     });
+  }
+
+  String _interpretIMC() {
+    if (_imc < 18.5) {
+      return 'Abaixo do peso';
+    } else if (_imc < 25) {
+      return 'Peso normal';
+    } else if (_imc < 30) {
+      return 'Sobrepeso';
+    } else if (_imc < 35) {
+      return 'Obesidade grau I';
+    } else if (_imc < 40) {
+      return 'Obesidade grau II';
+    } else {
+      return 'Obesidade grau III';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Calculadora de IMC'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'clique no botão',
+          children: [
+            TextField(
+              controller: _heightController,
+              decoration: InputDecoration(
+                labelText: 'Altura (cm)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: _weightController,
+              decoration: InputDecoration(
+                labelText: 'Peso (kg)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _calculateIMC,
+              child: Text('Calcular'),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Seu IMC é ${_imc.toStringAsFixed(1)}',
+              style: TextStyle(fontSize: 24),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _interpretIMC(),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.remove),
       ),
     );
   }
